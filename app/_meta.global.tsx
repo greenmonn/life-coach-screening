8fbd39558ee7bf331b9e5e0fc1ff345295cdd388
Juyeon <juyeon.yoon@kaist.ac.kr>
2025-11-19 11:58:29 +0900

    Update screening survey instructions

--- app/_meta.global.tsx
+++ app/_meta.global.tsx
@@ -1,46 +1,13 @@
 import {MetaRecord} from "nextra";
 
-const DOCS_ITEMS: MetaRecord = {
-    '---description': {
-        type: 'separator',
-        title: '연구 설명문 확인'
-    },
-    index: '',
-    description_2_target: '',
-    description_3_method: '',
-    description_4_period: '',
-    description_5_withdraw: '',
-    description_6_side_effects: '',
-    description_7_benefit: '',
-    description_8_disadvantage: '',
-    description_9_compensation: '',
-    description_10_privacy: '',
-    description_11_inquiry: '',
-    '---agreement': {
-        type: 'separator',
-        title: '연구 동의'
-    },
-    agreement_1: '',
-    further_process: '',
-}
-
 
 export default {
     index: {
         type: 'page',
         theme: {
-            layout: 'full',
+            layout: 'default',
             toc: false,
             timestamp: false,
         }
-    },
-    docs: {
-        type: 'page',
-        title: 'Check-in',
-        items: DOCS_ITEMS,
-        theme: {
-            timestamp: false,
-            collapsed: true,
-        }
-    },
+    }
 }
